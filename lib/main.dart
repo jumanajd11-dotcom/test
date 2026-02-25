58fb7ee8c3886dacf6fc48d1516e4e6fbcdd8043
BasmaAlhusayni <tu4358236@taibahu.edu.sa>
2026-02-25 03:48:33 +0300

    resolve error

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'this is a test'
+      title: 'this is a test',
 
     theme: ThemeData(
         // This is the theme of your application.
